79337205baca7616d72d01a53d80fb20fdf15329
ChinCluBi <chincub@gmail.com>
2015-08-22 01:04:40 +0700

    Fix bug

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,49 +2,49 @@
 var appControl = angular.module('appController', [])
 
 appControl.controller('preRegisterController', ['$scope', '$http' , '$state' , '$stateParams' , function ($scope, $http, $state, $stateParams) {
-  $scope.currentUI = 0;
-  $scope.message = 'Welcome to Freshy Camp Registration.';
-  $scope.state = [];
-  $scope.name="";
-  $scope.names = [];
-  $scope.all;
-  $http.get("http://localhost:3000/all")
-    .success(function(names){
-      // console.log(names);
-      $scope.all=names;
-      names.forEach(function(name){
-        $scope.names.push(name.name);
+  $scope.currentUI = 0
+  $scope.message = 'Welcome to Freshy Camp Registration.'
+  $scope.state = []
+  $scope.name = ''
+  $scope.names = []
+  $scope.all
+  $http.get('http://localhost:3000/all')
+    .success(function (names) {
+      // console.log(names)
+      $scope.all = names
+      names.forEach(function (name) {
+        $scope.names.push(name.name)
       })
-    });
-  $scope.setCurrentUI = function(current){
-    $scope.currentUI = current;
-    if(current==1){
-      $scope.all.some(function(name){
-        if(name.name == $scope.name){
-          $http.get("http://localhost:3000/"+name.student_id )
-          .success(function(object){
-            $scope.person = object[0];
-          })
+    })
+  $scope.setCurrentUI = function (current) {
+    $scope.currentUI = current
+    if (current == 1) {
+      $scope.all.some(function (name) {
+        if (name.name == $scope.name) {
+          $http.get('http://localhost:3000/' + name.student_id)
+            .success(function (object) {
+              $scope.person = object[0]
+            })
 
-          return true;
+          return true
         }
-      })  
+      })
     }
-    if(current == 2 ){
-      $http.post('localhost:3000/register', {id:$scope.person.student_id}).
-        then(function(response) {
-          console.log("hey");
-          // this callback will be called asynchronously
-          // when the response is available
-        }, function(response) {
-          console.log("fuck");
-          // called asynchronously if an error occurs
-          // or server returns response with an error status.
-        });
+    if (current == 2) {
+      $http.post('http://localhost:3000/register', {id: $scope.person.student_id}).
+        then(function (response) {
+          console.log('hey')
+        // this callback will be called asynchronously
+        // when the response is available
+        }, function (response) {
+          console.log('fuck')
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+        })
     }
-    
+
   }
 
-  
-  
+
+
 }])
