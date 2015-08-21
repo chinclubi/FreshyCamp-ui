ecedee8b63968570ea8e79117ba80c028cf163ed
smartCookly <worapon@cookly.me>
2015-08-22 03:48:57 +0700

    fix bug

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,9 +5,11 @@ appControl.controller('preRegisterController', ['$scope', '$http' , '$state' , '
   $scope.currentUI = 0
   $scope.message = 'Welcome to Freshy Camp Registration.'
   $scope.state = []
+  $scope.colors=['แดง' ,'น้ำเงิน' ,'เขียว' ,'เหลือง' ,'แสด' ,'น้ำตาล']
   $scope.name = ''
   $scope.names = []
   $scope.all
+  $scope.colorClass = "";
   $http.get('http://localhost:3000/all')
     .success(function (names) {
       // console.log(names)
@@ -16,14 +18,21 @@ appControl.controller('preRegisterController', ['$scope', '$http' , '$state' , '
         $scope.names.push(name.name)
       })
     })
+  $scope.checkColor = function(color){
+    if(color==0)
+      $scope.colors
+  }
+
+
   $scope.setCurrentUI = function (current) {
-    $scope.currentUI = current
     if (current == 1) {
+      $scope.message="Is this you?"
       $scope.all.some(function (name) {
         if (name.name == $scope.name) {
           $http.get('http://localhost:3000/' + name.student_id)
             .success(function (object) {
               $scope.person = object[0]
+              console.log($scope.person.color)
             })
 
           return true
@@ -31,9 +40,13 @@ appControl.controller('preRegisterController', ['$scope', '$http' , '$state' , '
       })
     }
     if (current == 2) {
+      $scope.message="Congratulation!"
+      console.log($scope.person.student_id);
       $http.post('http://localhost:3000/register', {id: $scope.person.student_id}).
         then(function (response) {
           console.log('hey')
+          $scope.getColor = response["data"][0];
+          $scope.colorClass = $scope.getColor.en_name;
         // this callback will be called asynchronously
         // when the response is available
         }, function (response) {
@@ -42,6 +55,9 @@ appControl.controller('preRegisterController', ['$scope', '$http' , '$state' , '
         // or server returns response with an error status.
         })
     }
+
+    $scope.currentUI = current
+    
 
   }
 
